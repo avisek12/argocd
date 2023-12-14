689531f45663770c9c57059ed48b95fda3fcbb54
avisek12 <109167023+avisek12@users.noreply.github.com>
2023-12-14 21:50:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
   # Configuration options
 }
   resource "aws_instance" "web" {
-  ami           = 
+  ami           = "ami-02a2af70a66af6dfb"
   instance_type = "t2.micro"
 
   tags = {
