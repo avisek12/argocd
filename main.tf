90e21fdac7e192d8a42e8382a5d043f52247f6e1
avisek12 <109167023+avisek12@users.noreply.github.com>
2023-12-14 21:52:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.30.0"
-    }
-  }
 provider "aws" {
   region = "ap-south-1"
   # Configuration options
